df51aa5b550676af5ec32d98c140234f20efcd6f
Nataliya772 <nsaxen@yandex.ru>
2020-06-22 00:21:38 +0300

    files_added

---
+++ application/db/people.py
@@ -0,0 +1,7 @@
+def get_employees():
+    empl_1 = 'Vasiliy'
+    empl_2 = 'Olga'
+    print(f'В отделе есть сотрудники {empl_1} и {empl_2}')
+
+if __name__ == '__main__':
+    get_employees()
---
+++ application/salary.py
@@ -0,0 +1,7 @@
+def calculate_salary():
+    Vasiliy = 30
+    Olga = 20
+    print(f'ЗП за месяц по отделу {Vasiliy + Olga}')
+
+if __name__ == '__main__':
+    calculate_salary()
